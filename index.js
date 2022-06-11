2a2311c06538cbaf24ddef08d9bc924c9853b0af
uyakaps <100175433+uyakaps@users.noreply.github.com>
2022-06-11 14:13:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3186,7 +3186,7 @@ let capt = `⭔ Title: ${judul}
             chika.sendImage(m.chat, thumb, capt, m)
             }
             break
-            case 'Jadwalbioskop': {
+            case 'forminfo': {
 
                 if (!q) return reply(`━━━༺ _*WELCOME*_ ༻━━━
 
