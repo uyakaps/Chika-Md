7d52167303da888bae598e07746c436bf001ba92
uyakaps <100175433+uyakaps@users.noreply.github.com>
2022-06-13 16:47:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3225,34 +3225,7 @@ Sudah Berapa Lama Main Toram :
    Form diatas cukup *SALIN>ISI>KIRIM* Tidak perlu di tulis ulang
    Form Diatas Diisi *Dengan Serius* Demi Kenyamanan Bersama.
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-Setelah Mengisi Form Harap Membaca Tentang Aturan Di Dalam Guild
-
-- Jaga *ATTITUDE*
-
-- Jangan *BAPERAN*
-
-- Dilarang *DRAMA*
-
-- Dilarang *SHARE HENTAI/PORNO*
-
-- Jangan Tampilkan *HARGA* berupa *Rupiah/IDR*(Wajib  PM Yang  *JUALAN*)
-
-- No Bahas Jenis *POLITIK,HOAX*
-
-- Dilarang bahas *MOD/CHEAT*
-
-- Jangan suka *SIDER DARI IN GAME DAN GRUP*
-
-- Tidak Menerima Akun *CLONE*
-
-- Tidak *ON* selama 7 hari *KICK*
-
-- *Izin PM LEAD/VICE* Jika ada kesibukan
-
-- *BISA MEMBACA*
-
-Hadir event Guild kalo gabisa hadir kasih tau ke *LEADER DAN VICE*
-Selamat Bergabung Dan Have Fun`)
+`)
 		    
 
             reply(mess.wait)
@@ -3563,7 +3536,7 @@ bagi yg mau join silahkan, gk mau gpp`)
             reply(capt)
             }
             break
-            case 'drakor': {
+            case 'rules': {
             if (!q) return reply(`Example : ${prefix + command} love`)
             reply(mess.wait)
             let res = await fetchJson(api('zenz', '/webzone/drakor', { query: text }, 'apikey'))
