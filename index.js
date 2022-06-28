883888b28e6d7b225480a83074998d426a24afd5
uyakaps <100175433+uyakaps@users.noreply.github.com>
2022-06-28 12:07:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1836,6 +1836,13 @@ case 'webtonsearch': case 'webtoon':
                     reply(mess.error)
                 })
             break
+			case 'kenapa':
+				if (!q) return reply(`Penggunaan ${command} text\n\nContoh : ${command} saya wibu`)
+					const kena = ['Karena lo wibu', 'Gk tau kak dia emang gitu', 'Off eregsi berat', 'Gk usah ngeluh mulu kak']
+					const pah = kena[Math.floor(Math.random() * kena.length)]
+chika.sendMessage(from, { text: `Pertanyaan : Apakah ${q}\nJawaban : ${pah}` }, { quoted: m })
+					
+break
             case 'waifu': case 'husbu': case 'neko': case 'shinobu': case 'megumin': case 'waifus': case 'nekos': case 'trap': case 'blowjob': {
                 reply(mess.wait)
                 chika.sendMessage(m.chat, { image: { url: api('zenz', '/api/random/'+command, {}, 'apikey') }, caption: 'Generate Random ' + command }, { quoted: m })
